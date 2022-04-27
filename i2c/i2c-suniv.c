84d4d0ebd09eea15735b4597a80960974f5fb5ce
IotaHydrae <writeforever@foxmail.com>
2022-04-28 07:11:26 +0800

    update
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

--- i2c/i2c-suniv.c
+++ i2c/i2c-suniv.c
@@ -1,5 +1,6 @@
 /*
  * Driver for the i2c controller on the suniv SoC family.
+ * This driver is based on i2c-mv64xxx.c
  *
  * Author: IotaHydrae <writeforever@foxmail.com>
  *
@@ -26,18 +27,38 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+#define SUNIV_I2C_REG_CLOCK_M(clkm) ((0xf & clkm) << 3)
+#define SUNIV_I2C_REG_CLOCK_N(clkn) (0x7 & clkn)
+
+/* suniv i2c control register bits */
+#define SUNIV_I2C_REG_CONTROL_INT_EN BIT(7)
+#define SUNIV_I2C_REG_CONTROL_BUS_EN BIT(6)
+#define SUNIV_I2C_REG_CONTROL_M_STA  BIT(5)
+#define SUNIV_I2C_REG_CONTROL_M_STP  BIT(4)
+#define SUNIV_I2C_REG_CONTROL_INT_FLAG BIT(3)
+#define SUNIV_I2C_REG_CONTROL_A_ACK  BIT(2)
+
+/* suniv i2c status register vals */
+#define SUNIV_I2C_BUS_STATUS_ERROR         0x00
+#define SUNIV_I2C_BUS_STATUS_START         0x08
+#define SUNIV_I2C_BUS_STATUS_REPEAT_START  0x10
+#define SUNIV_I2C_BUS_STATUS_ADDR_WR_ACK   0x18
+#define SUNIV_I2C_BUS_STATUS_ADDR_WR_NOACK 0x20
+#define SUNIV_I2C_BUS_STATUS_MASTER_DATA_ACK 0x28
+
+
 #define SUNIV_CONTLR_NAME "suniv_i2c"
 
 struct suniv_i2c_regs {
-	u8 addr;
-	u8 xaddr;
-	u8 data;
-	u8 cntr;
-	u8 stat;
-	u8 ccr;
-	u8 srst;
-	u8 efr;
-	u8 lcr;
+	u8 addr;	/* TWI Slave Address Register */
+	u8 xaddr;	/* TWI Extend Address Register */
+	u8 data;	/* TWI Data Register */
+	u8 cntr;	/* TWI Control Register */
+	u8 stat;	/* TWI Status Register */
+	u8 ccr;		/* TWI Clock Register */
+	u8 srst;	/* TWI Soft Reset Register */
+	u8 efr;		/* TWI Enhance Feature Register */
+	u8 lcr;		/* TWI Line Control Register */
 };
 
 struct suniv_i2c_data {
@@ -53,22 +74,44 @@ struct suniv_i2c_data {
 };
 
 struct suniv_i2c_regs suniv_i2c_regs_f1c100s = {
-	.addr  = 0x00,	/* TWI Slave Address Register */
-	.xaddr = 0x04,	/* TWI Extend Address Register */
-	.data  = 0x08,	/* TWI Data Register */
-	.cntr  = 0x0c,	/* TWI Control Register */
-	.stat  = 0x10,	/* TWI Status Register */
-	.ccr   = 0x14,	/*  */
+	.addr  = 0x00,
+	.xaddr = 0x04,
+	.data  = 0x08,
+	.cntr  = 0x0c,
+	.stat  = 0x10,
+	.ccr   = 0x14,
 	.srst  = 0x18,
 	.efr   = 0x1c,
 	.lcr   = 0x20,
 };
 
+static void suniv_i2c_hw_init(struct suniv_i2c_data *i2c_data)
+{
+	/*
+	 * we don't need to reset bus here, because
+	 * the reset system should do this work
+	 */
+
+	/* set the bus clock, temporarily set to 100Kbit/s */
+	writel(SUNIV_I2C_REG_CLOCK_N(2) | SUNIV_I2C_REG_CLOCK_M(11),
+		   i2c_data->base + i2c_data->reg_offsets.ccr);
+
+	/* clear addr,xaddr registers */
+	writel(0, i2c_data->base + i2c_data->reg_offsets.addr);
+	writel(0, i2c_data->base + i2c_data->reg_offsets.xaddr);
+
+	/* enable irq and bus ... */
+	writel(0, )
+}
+
+/* send i2c bus start signal */
 static void suniv_i2c_send_start(struct suniv_i2c_data *i2c_data)
 {
 	
+	printk(KERN_WARNING "%s, sending start signal\n", __func__);
 }
 
+/* suniv i2c bus intr handler */
 static irqreturn_t suniv_i2c_isr(int irq, void *dev_id)
 {
 	return IRQ_HANDLED;
@@ -81,8 +124,16 @@ static irqreturn_t suniv_i2c_isr(int irq, void *dev_id)
 static int suniv_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg *msgs,
 			   				  int num)
 {
-	for(int i = 0; i<msgs->len;i++) {
-		
+	int index;
+	struct suniv_i2c_data *i2c_data = i2c_get_adapdata(adap);
+	
+	/* test i2c start signal */
+	suniv_i2c_send_start(i2c_data);
+
+
+	/* do simple i2c msg loop */
+	for(index = 0; index<msgs->len;index++) {
+		printk(KERN_WARNING "prepared send to %d\n", msgs[index].addr);
 	}
 	return 0;
 }
@@ -90,9 +141,11 @@ static int suniv_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg *msgs,
 /* To determine what the adapter supports */
 static u32 suniv_i2c_functionality(struct i2c_adapter *adap)
 {
+	/* we don't need 10bit slave address now */
 	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_EMUL;
 }
 
+/* the smbus function could simulate by the i2c master_xfer func */
 static const struct i2c_algorithm suniv_i2c_algo = {
 	.master_xfer   = suniv_i2c_xfer,
 	.functionality = suniv_i2c_functionality,
@@ -109,33 +162,40 @@ static int suniv_i2c_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
+	/* ioremap the bus register base addr */
 	i2c_data->base = devm_platform_ioremap_resource(pdev, 0);
 	if(IS_ERR(i2c_data->base)){
 		return PTR_ERR(i2c_data->base);
 	}
 
+	/* init locks */
 	init_waitqueue_head(&i2c_data->wait_queue);
 	spin_lock_init(&i2c_data->lock);
 
+	/* get irq number from device */
 	i2c_data->irq = platform_get_irq(pdev, 0);
 	if(i2c_data->irq < 0){
 		return -EINVAL;
 	}
 
+	/* copy regs offset to self data */
 	memcpy(&i2c_data->reg_offsets, &suniv_i2c_regs_f1c100s, sizeof(struct suniv_i2c_regs));
 
+	/* setting i2c adapter structure */
 	i2c_data->adapter.owner       = THIS_MODULE;
 	i2c_data->adapter.dev.parent  = &pdev->dev;
 	i2c_data->adapter.algo        = &suniv_i2c_algo;
 	i2c_data->adapter.nr          = pdev->id;
 	i2c_data->adapter.dev.of_node = pdev->dev.of_node;
 
+	/* set privte data */
 	platform_set_drvdata(pdev, i2c_data);
 	i2c_set_adapdata(&i2c_data->adapter, i2c_data);
 
 	ret = devm_request_irq(&pdev->dev, i2c_data->irq, suniv_i2c_isr, 0, 
 						   SUNIV_CONTLR_NAME "adapter", i2c_data);
-	
+
+	/* do last work, add adapter to system */
 	if(ret) {
 		dev_err(&pdev->dev, "Failed to request irq %d\n", i2c_data->irq);
 		return ret;
@@ -145,7 +205,7 @@ static int suniv_i2c_probe(struct platform_device *pdev)
 	}
 
 err_free_irq:
-	free_irq(drv_data->irq, i2c_data);
+	free_irq(i2c_data->irq, i2c_data);
 
 	return ret;
 }
@@ -173,6 +233,7 @@ static struct platform_driver suniv_i2c_driver = {
 
 module_platform_driver(suniv_i2c_driver);
 
+MODULE_AUTHOR("Mark A. Greer <mgreer@mvista.com>");
 MODULE_AUTHOR("IotaHydrae writeforever@foxmail.com");
 MODULE_DESCRIPTION("Suniv SoC family host bridge i2c adapter driver");
 MODULE_LICENSE("GPL");
